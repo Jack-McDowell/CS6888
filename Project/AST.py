6a15bc994c2ced699a799392a98e3b4afefdb9b0
Jack McDowell <jack@mcdowell.tv>
2020-11-30 13:20:48 -0500

    Add untested AST conversion

---
+++ Project/AST.py
@@ -0,0 +1,27 @@
+import Operator, ExprType
+
+class ASTNode:
+    def __init__(self, operator, operands):
+        assert(len(operands) == operator.operands)
+
+        self.operator = operator
+        self.operands = operands
+        self.type = None
+        self.string_representation = None
+    
+    def get_sym(self, state):
+        return self.operator.angrify(self.operands, state)
+
+    def stringify(self):
+        if self.string_representation == None:
+            children = [child.stringify() for child in self.operands]
+            self.string_representation = self.operator.output(children)
+        
+        return self.string_representation
+
+    def get_type(self):
+        if self.type == None:
+            children = [child.get_type() for child in self.operands]
+            self.type = self.operator.typer(children)
+        
+        return self.type
---
+++ Project/Operator.py
@@ -0,0 +1,154 @@
+from enum import Enum
+
+class Type(Enum):
+    BOOL = 0
+    BV8 = 1
+    BV16 = 2
+    BV32 = 3
+    BV64 = 4
+
+class ExprType:
+    def __init__(self, t, pointers):
+        self.t = t
+        self.pointers = pointers
+
+    def get_pointed_size(self):
+        assert(self.pointers > 0)
+        assert(not self.t == Type.BOOL)
+
+        if self.pointers > 1:
+            return 64
+        elif self.pointers == 1:
+            return 4 * (2 ** self.t)
+
+def bigger_int(type1, type2):
+    assert(type1.pointers == 0 and not type1.t == Type.BOOL)
+    assert(type2.pointers == 0 and not type2.t == Type.BOOL)
+
+    return type1 if type1.t > type2 else type2
+
+def deref_type(type1):
+    assert(type1.pointers > 0)
+
+    return ExprType(type1.t, type1.pointers - 1)
+
+class Operator:
+    def __init__(self, output, angrify, typer, operands):
+        self.output = output
+        self.angrify = angrify
+        self.typer = typer
+        self.operands = operands
+
+    PLUS = Operator(
+        lambda operands: "(" + operands[0] + " + " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) + operands[1].get_sym(state),
+        lambda operands: bigger_int(operands[0], operands[1]),
+        2)
+
+    MINUS = Operator(
+        lambda operands: "(" + operands[0] + " - " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) - operands[1].get_sym(state),
+        lambda operands: bigger_int(operands[0], operands[1]),
+        2)
+
+    TIMES = Operator(
+        lambda operands: "(" + operands[0] + " * " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) * operands[1].get_sym(state),
+        lambda operands: bigger_int(operands[0], operands[1]),
+        2)
+
+    DIVIDE = Operator(
+        lambda operands: "(" + operands[0] + " / " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) / operands[1].get_sym(state),
+        lambda operands: bigger_int(operands[0], operands[1]),
+        2)
+
+    EXP = Operator(
+        lambda operands: "(" + operands[0] + " ** " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) ** operands[1].get_sym(state),
+        lambda operands: bigger_int(operands[0], operands[1]),
+        2)
+
+    BAND = Operator(
+        lambda operands: "(" + operands[0] + " & " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) & operands[1].get_sym(state),
+        lambda operands: bigger_int(operands[0], operands[1]),
+        2)
+
+    BOR = Operator(
+        lambda operands: "(" + operands[0] + " | " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) | operands[1].get_sym(state),
+        lambda operands: bigger_int(operands[0], operands[1]),
+        2)
+
+    BXOR = Operator(
+        lambda operands: "(" + operands[0] + " ^ " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) | operands[1].get_sym(state),
+        lambda operands: bigger_int(operands[0], operands[1]),
+        2)
+
+    EQ = Operator(
+        lambda operands: "(" + operands[0] + " == " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) == operands[1].get_sym(state),
+        lambda operands: Type.BOOL,
+        2)
+
+    NEQ = Operator(
+        lambda operands: "(" + operands[0] + " != " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) != operands[1].get_sym(state),
+        lambda operands: Type.BOOL,
+        2)
+
+    GT = Operator(
+        lambda operands: "(" + operands[0] + " > " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) > operands[1].get_sym(state),
+        lambda operands: Type.BOOL,
+        2)
+
+    LT = Operator(
+        lambda operands: "(" + operands[0] + " < " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) < operands[1].get_sym(state),
+        lambda operands: Type.BOOL,
+        2)
+
+    GE = Operator(
+        lambda operands: "(" + operands[0] + " >= " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) >= operands[1].get_sym(state),
+        lambda operands: Type.BOOL,
+        2)
+
+    LE = Operator(
+        lambda operands: "(" + operands[0] + " <= " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) <= operands[1].get_sym(state),
+        lambda operands: Type.BOOL,
+        2)
+
+    LOR = Operator(
+        lambda operands: "(" + operands[0] + " || " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) or operands[1].get_sym(state),
+        lambda operands: Type.BOOL,
+        2)
+
+    LAND = Operator(
+        lambda operands: "(" + operands[0] + " && " + operands[1] + ")",
+        lambda operands, state: operands[0].get_sym(state) and operands[1].get_sym(state),
+        lambda operands: Type.BOOL,
+        2)
+
+    DEREF = Operator(
+        lambda operands: "*(" + operands[0] + ")",
+        lambda operands, state: 
+            state.memory.load(operands[0].get_sym(state), 
+                              operands[0].get_type().get_pointed_size(),
+                              disable_actions=True, inspect=False)
+        lambda operands: deref_type(operands[0]),
+        1)
+
+    INDEX = Operator(
+        lambda operands: "&(" + operands[0] + ")",
+        lambda operands, state: 
+            state.memory.load(operands[0].get_sym(state) + operands[0].get_type().get_pointed_size() * operands[1].get_sym(state), 
+                              operands[0].get_type().get_pointed_size(),
+                              disable_actions=True, inspect=False)
+        lambda operands: deref_type(operands[0]),
+        2)
