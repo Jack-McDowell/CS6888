2606148fe0c9a2f3193ab8925e34aa75cd1fa19d
Will <willmayes1999@gmail.com>
2020-12-04 13:52:46 -0500

    Added functionality to get bounds of a function

--- Project/elfparse.py
+++ Project/elfparse.py
@@ -1,9 +1,27 @@
+cfg = {}
+func_bounds = {}
+
+
 def get_function_bounds(project, function_name):
     """
     Returns (start address, end address) for the given function in
     the ANGR project passed as an argument.
     """
-    pass
+    id_tup = (project, function_name)
+    global cfg
+    global func_bounds
+    if project not in cfg:
+        cfg = project.analyses.CFGFast()
+    if id_tup in func_bounds:
+        return func_bounds[id_tup]
+    func = cfg.kb.functions.function(name=function_name)
+    end_addr = 0
+    for block in func.blocks:
+        if block.instruction_addrs[block.instructions - 1] > end_addr:
+            end_addr = block.instruction_addrs[block.instructions - 1]
+    func_bounds[id_tup] = (func.addr, end_addr)
+    return func_bounds[id_tup]
+
 
 def get_var_stack_offset(project, function_name, var_name):
     """
